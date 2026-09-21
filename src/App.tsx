8c7e65b47a11ea125cf26fd061dc492341e1e952
loremus <loremus@proton.me>
2026-09-21 17:25:02 +0530

    shadcn setup :3

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,9 @@
+import { Button } from "./components/ui/button";
+
 export default function App() {
-  return <div className="text-2xl p-4">Hi</div>;
+  return (
+    <div className="text-2xl p-4">
+      Hi <Button>A button :3</Button>
+    </div>
+  );
 }
