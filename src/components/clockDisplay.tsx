2f5de984f83cf5801946b5a135b7e218ff384e9f
loremus <loremus@proton.me>
2026-09-24 11:08:49 +0530

    clock format default

--- src/components/clockDisplay.tsx
+++ src/components/clockDisplay.tsx
@@ -1,19 +1,31 @@
+/* eslint-disable react-hooks/set-state-in-effect */
 import { useEffect, useState } from "react";
+import dayjs from "dayjs";
 
 export function ClockDisplay() {
   const [time, setTime] = useState(new Date());
+  const [format, setFormat] = useState(() => {
+    let f = localStorage.getItem("clock-format");
+    if (f == "" || !f) {
+      localStorage.setItem("clock-format", "HH:mm:ss");
+      f = "HH:mm:ss";
+    }
+
+    return f;
+  });
 
   useEffect(() => {
     const timer = setInterval(() => {
       setTime(new Date());
     }, 1000);
 
+    setFormat(localStorage.getItem("clock-format")!);
     return () => clearInterval(timer);
   }, []);
 
   return (
     <div className="border rounded-md w-full min-h-20 h-full grid place-items-center bg-primary font-mono text-background text-3xl tracking-tighter">
-      {`${time.getHours().toLocaleString().padStart(2, "0")}:${time.getMinutes().toLocaleString().padStart(2, "0")}:${time.getSeconds().toLocaleString().padStart(2, "0")}`}
+      {dayjs(time).format(format)}
     </div>
   );
 }
