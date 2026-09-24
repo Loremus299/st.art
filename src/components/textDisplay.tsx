95e42d31c5d025639f63b2adeb34a25378e2a934
loremus <loremus@proton.me>
2026-09-24 17:55:12 +0530

    yay

--- src/components/textDisplay.tsx
+++ src/components/textDisplay.tsx
@@ -16,7 +16,7 @@ export default function TextDisplay({
             localStorage.setItem(id, e.currentTarget.value);
           }}
           placeholder={p!}
-          className="rounded-md w-full h-full min-h-20 text-background dark:text-primary dark:border-primary "
+          className="rounded-md w-full h-full min-h-20 text-background dark:text-primary dark:border-primary"
         />
       ) : (
         <div className="w-full h-full text-lg text-pretty backdrop-blur-md text-background border min-h-20  dark:text-primary dark:border-primary rounded-md grid place-items-center">
